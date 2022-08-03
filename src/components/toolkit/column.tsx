cf5e63329022555a708a272e4576eaeae6fb2764
fsvboas <fsvboas.dev@gmail.com>
2022-08-03 18:17:49 -0300

    feat: create toolkit components

---
+++ src/components/toolkit/column.tsx
@@ -0,0 +1,22 @@
+import { classNames } from '../../core/helpers/class-names'
+import React from 'react'
+
+export type ColumnElementType = 'div' | 'section'
+export interface ColumnProps extends React.HTMLAttributes<HTMLDivElement> {
+  as?: ColumnElementType
+}
+
+const Column: React.FC<ColumnProps> = ({
+  className,
+  as = 'div',
+  children,
+  ...props
+}) => {
+  return React.createElement(
+    as,
+    { ...props, className: classNames('flex flex-col', className) },
+    children
+  )
+}
+
+export default Column
