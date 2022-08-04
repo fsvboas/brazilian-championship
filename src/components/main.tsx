de5f0fa4f63a9a270cd12af50c3b8c403148c9fc
fsvboas <fsvboas.dev@gmail.com>
2022-08-04 01:30:32 -0300

    refactor: adjust some styles and add stats tab

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { classNames } from '../core/helpers/class-names'
+import StatsTab from './tabs/stats/stats'
+import Stats from './tabs/stats/stats'
 import Column from './toolkit/column'
+import Image from './toolkit/image'
 import Row from './toolkit/row'
 
 const Main: React.FC = ({}) => {
@@ -8,10 +11,17 @@ const Main: React.FC = ({}) => {
 
   return (
     <Column className="h-full bg-secondary">
-      <Column className="bg-primary space-y-8">
-        <Row className="text-light p-4">Brasileirão Série B</Row>
+      <Column className="bg-primary space-y-2 border-b-4 border-dark shadow-sm">
+        <Row className="text-light p-4 space-x-2">
+          <Image
+            className="w-6"
+            src="https://upload.wikimedia.org/wikipedia/pt/f/f4/Campeonato_Brasileiro_S%C3%A9rie_B_logo.png"
+            alt=""
+          />
+          <h1 className="font-medium">Brasileirão Série B</h1>
+        </Row>
         <nav className="flex justify-center">
-          <ul className="flex space-x-8 w-full md:w-1/2 justify-between">
+          <ul className="flex w-full lg:w-[40%] justify-between uppercase font-medium text-sm">
             <li
               className={classNames(
                 'w-40 text-center cursor-pointer text-light/80 p-2',
@@ -68,13 +78,13 @@ const Main: React.FC = ({}) => {
         </nav>
       </Column>
       <Row className="flex justify-center">
-        <Row className="bg-dark/50 w-full md:w-1/2 h-screen justify-center">
+        <Row className="bg-dark w-full lg:w-[40%] h-screen justify-center">
           {activeTab === 'matches' ? (
             <p>Tab de partidas</p>
           ) : activeTab === 'standings' ? (
             <p>Tab de posições</p>
           ) : activeTab === 'stats' ? (
-            <p>Tab de estatísticas</p>
+            <StatsTab />
           ) : activeTab === 'players' ? (
             <p>Tab de jogadores</p>
           ) : (
